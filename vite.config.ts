f2bbbedb87236dd4fe08e94718d185c90cca2f06
vasanth-tech-byclarity <vasanthkr@byclaritytech.com>
2025-09-08 19:00:33 +0530

    Fix GitHub Pages routing

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command  }) => ({
+export default defineConfig(({ command }) => ({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: command === "build" ? "/portfolio-site/" : "/", // GitHub Pages base path
   server: {
     host: "::",
     port: 8080,
-  },
-  base: command === 'build' ? '/portfolio-site/' : '/', 
-  plugins: [
-    react(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"), 
-    },
   },
 }));
